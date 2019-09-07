e09c13a655b01037b9979eafc42bc8b2e6e09dbc
Benno Staubexitquit <benno.staub@hotmail.com>
2019-09-07 14:08:58 +0200

    Add solution for 1.6

---
+++ CrackingTheCodingInterview/Chapter1/stringCompression.py
@@ -0,0 +1,19 @@
+def stringCompression(string):
+	new_string = ''
+	take_new = False
+	count_vector = [0 for _ in xrange(26)]
+	for char in string:
+		index = ord(char) - ord('a')
+		count_vector[index] += 1
+	for index in xrange(len(count_vector)):
+		if count_vector[index] > 0:
+			new_string = new_string + chr(index + ord('a')) + str(count_vector[index])
+	if len(new_string) < len(string):
+		return new_string
+	else:
+		return string
+
+
+if __name__ == '__main__':
+	string = 'abcdefghijksasfasfafafsfaergsggsdsafasfsmnopqrstuvwxyzf'
+	print stringCompression(string)
