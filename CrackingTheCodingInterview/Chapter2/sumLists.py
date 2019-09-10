5c7d30c05e5f8c2bc1757dd6869188ea9dac3015
Benno Staubexitquit <benno.staub@hotmail.com>
2019-09-10 23:11:51 +0200

    Fixed edge cases for solution 2.5

--- CrackingTheCodingInterview/Chapter2/sumLists.py
+++ CrackingTheCodingInterview/Chapter2/sumLists.py
@@ -24,18 +24,36 @@ def sumLists(head1, head2):
 		head2 = head2.next
 	while head1:
 		s.next = head1
+		if add_next:
+			if s.next.value + 1 < 10:
+				s.next.value += 1
+				add_next = 0
+			else:
+				s.next.value -= 9
+				add_next = 1
 		head1 = head1.next
+		s = s.next
 	while head2:
 		s.next = head2
+		if add_next:
+			if s.next.value + 1 < 10:
+				s.next.value += 1
+				add_next = 0
+			else:
+				s.next.value -= 9
+				add_next = 1
 		head2 = head2.next
+		s = s.next
+	if add_next:
+		s.next = Node(1)
 	printLinkedList(sumlist)
 
 
 
 
 if __name__ == '__main__':
-	head1 = initiateLinkedList([7,1,6])
-	head2 = initiateLinkedList([5,9,2])
+	head1 = initiateLinkedList([7,1,9])
+	head2 = initiateLinkedList([5,9,2,9])
 	printLinkedList(head1)
 	print('second')
 	printLinkedList(head2)
