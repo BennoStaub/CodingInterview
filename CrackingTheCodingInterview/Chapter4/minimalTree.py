ae930534416cd45a66f3bf7216176582c744fe60
Benno Staubexitquit <benno.staub@hotmail.com>
2019-09-12 22:46:54 +0200

    Test .gitignore

--- CrackingTheCodingInterview/Chapter4/minimalTree.py
+++ CrackingTheCodingInterview/Chapter4/minimalTree.py
@@ -3,5 +3,5 @@ sys.path.append('../../DataStructures/')
 from graph import Node, Graph, initiateGraph, initiateBinarySearchTree
 
 graph = Graph()
-initiateBinarySearchTree(graph, [1,3,4,6,9,12,15,19,22,25])
+initiateBinarySearchTree(graph, [1,3,4,6,9,12,15,19,22,25,30,38])
 graph.printGraph()
